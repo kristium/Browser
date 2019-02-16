5a0dd49c44e61214360fe68d98ee8975ee52ff36
Massimo Giardina <massimoG12345@users.noreply.github.com>
2019-02-16 20:04:09 +0100

    Add files via upload

--- client.lua
+++ client.lua
@@ -1,12 +1,55 @@
 --[[Kristium browser client]]--
-local function sp(x,y,t) return t and t.setCursorPos(x,y) or term.setCursorPos(x,y) end
-local function fc(col,t) return t and t.setTextColour(col) or term.setTextColour(col) end
-local function bc(col,t) return t and t.setBackgroundColour(col) or term.setBackgroundColour(col) end
+
+local node = "https://kristium.ale32bit.me/"
+local version = 1.0
+
+local sandbox = {
+	term = term,
+	math = math,
+	assert = assert,
+	bit = bit,
+	bit32 = bit32,
+	colors = colors,
+	colours = colours,
+	http = http,
+	ipairs = ipairs,
+	pairs = pairs,
+	json = json,
+	keys = keys,
+	next = next,
+	paintutils = paintutils, -- unsafe
+	pcall = pcall,
+	xpcall = xpcall,
+	print = print,
+	printError = printError,
+	read = read,
+	sleep = sleep,
+	table = table,
+	textutils = textutils,
+	type = type,
+	unpack = unpack,
+	vector = vector,
+	write = write,
+	string = string,
+	tostring = tostring,
+	tonumber = tonumber,
+}
+
+sandbox.browser.sp = function(x,y,t) return t and t.setCursorPos(x,y) or term.setCursorPos(x,y) end
+sandbox.browser.fc = function(col,t) return t and t.setTextColour(col) or term.setTextColour(col) end
+sandbox.browser.bc = function(col,t) return t and t.setBackgroundColour(col) or term.setBackgroundColour(col) end
+sandbox.browser.write = function(str,t) return t and t.write(str) or term.write(str) end
 
 local function center(str,y,t)
   local w,h = t and t.getSize() or term.getSize()
-  sp(math.floor(w/2-(#str/2)),y,t)
-  if t then t.write(str) else term.write(str) end
+  sandbox.browser.sp(math.floor(w/2-(#str/2)),y,t)
+  sandbox.browser.write(str)
 end
 
-local sandbox = {}
+
+sandbox.browser = {
+	get = function(url)
+
+	end,
+	useragent = "Kristium "..version.." (".._HOST..")",
+}
