12df5e0e6cf15e5abdb45031c6322887c871111a
Massimo Giardina <massimoG12345@users.noreply.github.com>
2019-02-16 19:57:59 +0100

    Add files via upload

--- client.lua
+++ client.lua
@@ -1,1 +1,12 @@
 --[[Kristium browser client]]--
+local function sp(x,y,t) return t and t.setCursorPos(x,y) or term.setCursorPos(x,y) end
+local function fc(col,t) return t and t.setTextColour(col) or term.setTextColour(col) end
+local function bc(col,t) return t and t.setBackgroundColour(col) or term.setBackgroundColour(col) end
+
+local function center(str,y,t)
+  local w,h = t and t.getSize() or term.getSize()
+  sp(math.floor(w/2-(#str/2)),y,t)
+  if t then t.write(str) else term.write(str) end
+end
+
+local sandbox = {}
